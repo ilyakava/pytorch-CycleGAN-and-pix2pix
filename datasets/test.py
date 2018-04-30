8315a80881cf7a0aed0fe3e59d9c760d92c12e9c
Ilya Kavalerov <ilyakavalerov@gmail.com>
2018-04-30 10:42:55 -0400

    WIP have 1 backprojection matching skimage iradon

--- datasets/test.py
+++ datasets/test.py
@@ -4,16 +4,21 @@ import scipy
 import scipy.misc
 import pdb
 
+from skimage.io import imread
+import matplotlib.pyplot as plt
+
 import torch
 import torch.autograd as autograd
 import torch.nn.functional as F
 
 # lena = scipy.misc.lena()
-obj = np.rand(256,256)
+obj = imread('/scratch0/ilya/locDoc/data/siim-medical-images/337/ID_0004_AGE_0056_CONTRAST_1_CT.png', as_grey=True)
+obj = np.random.rand(256,256)
 obj.shape
 ang = np.linspace(0., 180., 50, endpoint=False)
 proj = radon(obj, theta=ang, circle=False)
 pdb.set_trace()
+# b 262
 rec = iradon(proj, theta=ang, circle=False)
 
 
@@ -23,22 +28,17 @@ autograd.Variable(torch.from_numpy(winO1.filters).type(dtype))
 #torch.nn.functional.grid_sample(input, grid, mode='bilinear', padding_mode='zeros')
 def torch_iradon():
     # t, x, radon_filtered[:, i]
-    xpr = X - int(output_size) // int(output_size)
-    ypr = Y - int(output_size) // int(output_size)
-    t = ypr * np.cos(th[i]) - xpr * np.sin(th[i])
+    
     
     preG = np.reshape(radon_filtered, (1,1)+radon_filtered.shape)
     radon_filteredG = autograd.Variable(torch.from_numpy(preG).type(dtype))
 
-    scaler = radon_filteredG.size(3) // 2
-    ty = t / scaler
-    txval = (i - radon_filteredG.size(3) // 2) / (radon_filteredG.size(3) // 2)
+    ty = t / (radon_filteredG.size(2) // 2)
+    txval = (i - radon_filteredG.size(2) // 2) / (radon_filteredG.size(2) // 2)
     tx = np.ones(ty.shape) * txval
-
     t4dim = np.zeros((1,)+ ty.shape + (2,))
     t4dim[0,:,:,0] = tx
     t4dim[0,:,:,1] = ty
-
     tG = autograd.Variable(torch.from_numpy(t4dim).type(dtype))
 
     # bilinear mode is effectively linear since we are not using the 2nd dimension
