a1036637d68fde3541d2548c67ec184d21c3e60f
Ilya Kavalerov <ilyakavalerov@gmail.com>
2018-05-01 22:00:26 -0400

    WIP Move ellipsoid sinogram npys according to A and B test/train/val splits

--- datasets/move_sinograms_by_AB_test_train_val.py
+++ datasets/move_sinograms_by_AB_test_train_val.py
@@ -6,26 +6,23 @@ import random as r
 
 import pdb
 
-A_SRC_DIR = '/scratch0/public/siim-medical-images/pix2pix_premerge'
-C_DEST_DIR = '/scratch0/public/siim-medical-images/pix2pix_premerge/C'
-C_SRC_DIR = '/scratch0/public/siim-medical-images/50_views_npys'
-
-# VAL_SZ = 10
-TEST_SZ = 10
+A_SRC_DIR = '/scratch0/public/ellipsoids/pix2pix_premerge/A'
+C_DEST_DIR = '/scratch0/public/ellipsoids/pix2pix_premerge/C'
+C_SRC_DIR = '/scratch0/public/ellipsoids/50_views_npys'
 
 train = os.listdir('%s/train' % A_SRC_DIR)
 val = os.listdir('%s/val' % A_SRC_DIR)
 test = os.listdir('%s/test' % A_SRC_DIR)
 
-os.system('mkdir %s/train' % C_DEST_DIR)
-os.system('mkdir %s/test' % C_DEST_DIR)
-os.system('mkdir %s/val' % C_DEST_DIR)
+os.system('mkdir -p %s/train' % C_DEST_DIR)
+os.system('mkdir -p %s/test' % C_DEST_DIR)
+os.system('mkdir -p %s/val' % C_DEST_DIR)
 
 for name in train:
-    os.system('cp %s/%s.npy %s/C/train/%s.npy' % (C_SRC_DIR, name, C_DEST_DIR, name))
+    os.system('cp %s/%s.npy %s/train/%s.npy' % (C_SRC_DIR, name, C_DEST_DIR, name))
 
 for name in test:
-    os.system('cp %s/%s.npy %s/C/test/%s.npy' % (C_SRC_DIR, name, C_DEST_DIR, name))
+    os.system('cp %s/%s.npy %s/test/%s.npy' % (C_SRC_DIR, name, C_DEST_DIR, name))
 
 for name in val:
-    os.system('cp %s/%s.npy %s/C/val/%s.npy' % (C_SRC_DIR, name, C_DEST_DIR, name))
+    os.system('cp %s/%s.npy %s/val/%s.npy' % (C_SRC_DIR, name, C_DEST_DIR, name))
